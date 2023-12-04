3826c786883cfe90b52a4bf5504591bca97a9586
unknown576 <62588524+unknown576@users.noreply.github.com>
2023-12-04 12:44:10 +0100

    Update AI_Tree_Structure.py

--- AI_Tree_Structure.py
+++ AI_Tree_Structure.py
@@ -11,8 +11,4 @@ class Branch:
         self.child_2 = child_2
         self.question = Branch_q
 
-    def get_offshoots(self):
-        """
-        Returns a tuple of a branches two childs in order (child_1, child_2).
-        """
-        return (self.child_1, self.child_2)
+
