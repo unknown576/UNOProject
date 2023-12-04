55e3b8201e8ba8bdd6d51d8d64e7eb25ba90d6f4
unknown576 <62588524+unknown576@users.noreply.github.com>
2023-12-04 12:40:18 +0100

    Update AI_Decision_Tree.py

--- AI_Decision_Tree.py
+++ AI_Decision_Tree.py
@@ -8,24 +8,24 @@ class CPUPlayer:
 
     def build_decision_tree(self):
         tree = Branch(
-            Branch_q=self.has_wild_draw_four,  # Branch question: Do we have a Wild Draw Four?
-            child_1=Leaf("Play Wild Draw Four"),  # Child 1: Yes, play it.
-            child_2=Branch(  # Child 2: No, check the next question.
-                Branch_q=self.has_skip_card,  # Do we have a card matching current color or number?
-                child_1=Leaf("Play Skip card"),  # Yes, play it.
-                child_2=Branch(  # No, check the next question: skip card, wild card, +2 card, reverse card, etc.
-                    Branch_q=self.has_matching_card(),  # Do we have a Skip card?
-                    child_1=Leaf("Play matching card"),  # Yes, play it.
-                    child_2=Branch(  # No, check for a Wild card.
-                        Branch_q=self.has_wild_card,  # Do we have a Wild card?
-                        child_1=Leaf("Play Wild card"),  # Yes, play it.
-                        child_2=Branch(  # No, check for a +2 card.
-                            Branch_q=self.has_plus_two_card,  # Do we have a +2 card?
-                            child_1=Leaf("Play +2 card"),  # Yes, play it.
-                            child_2=Branch(  # No, check for a Reverse card.
-                                Branch_q=self.has_reverse_card,  # Do we have a Reverse card?
-                                child_1=Leaf("Play Reverse card"),  # Yes, play it.
-                                child_2=Leaf("Draw card")  # No specific cards to play, draw a card.
+            Branch_q=self.has_wild_draw_four,
+            child_1=Leaf("Play Wild Draw Four"),
+            child_2=Branch(
+                Branch_q=self.has_wild_card,
+                child_1=Leaf("Play Wild card"),
+                child_2=Branch(
+                    Branch_q=self.has_plus_two_card,
+                    child_1=Leaf("Play +2 card"),
+                    child_2=Branch(
+                        Branch_q=self.has_reverse_card,
+                        child_1=Leaf("Play Reverse card"),
+                        child_2=Branch(
+                            Branch_q=self.has_skip_card,
+                            child_1=Leaf("Play Skip card"),
+                            child_2=Branch(
+                                Branch_q=self.has_matching_card,
+                                child_1=Leaf("Play matching card"),
+                                child_2=Leaf("Draw card")
                             )
                         )
                     )
