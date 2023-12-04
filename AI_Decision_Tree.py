e7522f7b1c89036a229e0866271eff9005ed6468
unknown576 <62588524+unknown576@users.noreply.github.com>
2023-12-04 12:26:37 +0100

    Update AI_Decision_Tree.py

--- AI_Decision_Tree.py
+++ AI_Decision_Tree.py
@@ -1,4 +1,4 @@
-from AI_Tree_Structure import  Branch, Leaf
+from AI_Tree_Structure import Branch, Leaf
 
 
 class CPUPlayer:
@@ -7,82 +7,55 @@ class CPUPlayer:
         self.hand = []
 
     def build_decision_tree(self):
-        # This is an overly simplified example. Build out a realistic tree based on UNO logic.
         tree = Branch(
             Branch_q=self.has_wild_draw_four,  # Branch question: Do we have a Wild Draw Four?
             child_1=Leaf("Play Wild Draw Four"),  # Child 1: Yes, play it.
             child_2=Branch(  # Child 2: No, check the next question.
                 Branch_q=self.has_matching_card,  # Do we have a card matching current color or number?
-                child_1=Leaf("Play matching card"),  # Yes, it.
-                child_2=Leaf("Draw card")  # No, draw a card.
-            )
-        )
-        # Add branches for other card types
-        tree = Branch(
-            Branch_q=self.has_skip_card,  # Branch question: Do we have a Skip card?
-            child_1=Leaf("Play Skip card"),  # Child 1: Yes, play it.
-            child_2=Branch(  # Child 2: No, check the next question.
-                Branch_q=self.has_wild_card,  # Do we have a Wild Card?
-                child_1=Leaf("Play Wild Card"),  # Yes, it.
-                child_2=Leaf("Draw card")  # No, draw a card.
-            )
-        )
-        tree = Branch(
-            Branch_q=self.has_plus_two_card,  # Branch question: Do we have a Plus Two card?
-            child_1=Leaf("Play Plus Two card"),  # Child 1: Yes, play it.
-            child_2=Branch(  # Child 2: No, check the next question.
-                Branch_q=self.has_reverse_card,  # Do we have a Reverse card?
-                child_1=Leaf("Play Reverse card"),  # Yes, it.
-                child_2=Leaf("Draw card")  # No, draw a card.
+                child_1=Leaf("Play matching card"),  # Yes, play it.
+                child_2=Branch(  # No, check the next question: skip card, wild card, +2 card, reverse card, etc.
+                    Branch_q=self.has_skip_card,  # Do we have a Skip card?
+                    child_1=Leaf("Play Skip card"),  # Yes, play it.
+                    child_2=Branch(  # No, check for a Wild card.
+                        Branch_q=self.has_wild_card,  # Do we have a Wild card?
+                        child_1=Leaf("Play Wild card"),  # Yes, play it.
+                        child_2=Branch(  # No, check for a +2 card.
+                            Branch_q=self.has_plus_two_card,  # Do we have a +2 card?
+                            child_1=Leaf("Play +2 card"),  # Yes, play it.
+                            child_2=Branch(  # No, check for a Reverse card.
+                                Branch_q=self.has_reverse_card,  # Do we have a Reverse card?
+                                child_1=Leaf("Play Reverse card"),  # Yes, play it.
+                                child_2=Leaf("Draw card")  # No specific cards to play, draw a card.
+                            )
+                        )
+                    )
+                )
             )
         )
         return tree
 
+    # Functions to check card availability can be implemented here.
+    # For example:
     def has_wild_draw_four(self):
-        # Logic to determine if we have a Wild Draw Four card
+        # Logic to check if the player has a Wild Draw Four card
         pass
 
     def has_matching_card(self):
-        # Logic to determine if we have a card matching current color or number?
+        # Logic to check if the player has a matching card
         pass
 
     def has_skip_card(self):
-        # Logic to determine if we have a Skip card
+        # Logic to check if the player has a Skip card
         pass
 
     def has_wild_card(self):
-        # Logic to determine if we have a Wild Card
+        # Logic to check if the player has a Wild card
         pass
 
     def has_plus_two_card(self):
-        # Logic to determine if we have a Plus Two card
+        # Logic to check if the player has a +2 card
         pass
 
     def has_reverse_card(self):
-        # Logic to determine if we have a Reverse card
+        # Logic to check if the player has a Reverse card
         pass
-
-    def traverse_tree_and_play(self, tree):
-        """
-        Traverse the decision tree and take the action at the leaf.
-        """
-        current = tree
-        while isinstance(current, Branch):  # Loop until a Leaf is reached
-            if current.question():  # Call the question method
-                current = current.child_1  # If True, follow child_1
-            else:
-                current = current.child_2  # If False, follow child_2
-        return self.execute_play(current.value)  # Current is a Leaf, execute the play
-
-    def execute_play(self, action):
-        """
-        Execute the action specified by the leaf.
-        """
-        # Logic to play the card or draw a card as dictated by the leaf's action
-        pass
-
-    def take_turn(self, current_color, current_value):
-        decision_tree = self.build_decision_tree()
-        return self.traverse_tree_and_play(decision_tree)
-
-# ... Other methods and game logic not shown for brevity ...
